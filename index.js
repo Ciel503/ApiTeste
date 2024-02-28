48936f75e21bf77015a57419540cea39e1f5512b
Franciel Mesquita <francielmsq@gmail.com>
2024-02-27 23:42:07 -0300

    mn

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const path = require('path');
 const app = express();
 const rota = express.Router();
 
-app.get("/",(req,res)=>{
+rota.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname + "/pages/home.html"))
 })
-app.get("/contato",(req,res)=>{
+rota.get("/contato",(req,res)=>{
     res.sendFile(path.join(__dirname + "/pages/contato.html"))
 })
 
-
+app.use(rota)
 const port = process.env.PORT || 3000;
 
 app.listen(port,(req,res)=>{
