8a7f8aeeec0ae77f5816c096b8551e7ae389dd7e
Franciel Mesquita <francielmsq@gmail.com>
2024-02-28 01:12:09 -0300

    nnnn

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-// const rota = express.Router();
-
-// rota.get("/",(req,res)=>{
-//     res.sendFile(path.join(__dirname + "/home.html"))
-// })
-// rota.get("/contato",(req,res)=>{
-//     res.sendFile(path.join(__dirname + "/contato.html"))
-// })
-
-// app.use(rota);
-// const port = process.env.PORT || 3000;
-
-// app.listen(port,(req,res)=>{
-//     console.log("servidor rodando")
-// })
 
 const express = require('express');
 const app = express();
