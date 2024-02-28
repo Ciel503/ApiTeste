f025ea42703beefb9c17dca12bc6432a151f09a0
Franciel Mesquita <106025802+Ciel503@users.noreply.github.com>
2024-02-27 21:27:45 -0300

    iniciando

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const rota = express.Router();
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname + "/pages/home.html"))
+})
+app.get("/contato",(req,res)=>{
+    res.sendFile(path.join(__dirname + "/pages/contato.html"))
+})
+
+
+
+app.listen(3000,(req,res)=>{
+    console.log("servidor rodando")
+})
